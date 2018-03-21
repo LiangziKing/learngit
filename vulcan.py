d64294979274dce42a6fcd0c2f716b175935398e
LiangziKing <1990818918@qq.com>
2018-03-21 16:56:24 +0800

    add feature vulcan

---
+++ vulcan.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-21 16:51:59
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-21 16:52:25
+
+print('5分钟后，开发完毕：')
