dc004899f4481be405f5701e69bbe7727bcbcbfd
LiangziKing <1990818918@qq.com>
2018-03-22 15:15:28 +0800

    add git-cheatsheet.pdf

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: LiangziKing
+# @E-mail: 1990818918@qq.com
+# @Date:   2018-03-22 10:24:27
+# @Last Modified by:   LiangziKing
+# @Last Modified time: 2018-03-22 10:24:53
+
+print ('hello ,world')
